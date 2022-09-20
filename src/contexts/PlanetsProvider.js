b0a52c7456eaf52fce6a2b42930a810ca170170b
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-20 20:39:24 -0300

    perf(Contexts): :zap: Coloca use effect direto no Provider

--- src/contexts/PlanetsProvider.js
+++ src/contexts/PlanetsProvider.js
@@ -1,25 +1,26 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import PlanetsContext from './PlanetsContext';
 import fetchPlanets from '../services/fetchPlanets';
 
 function PlanetsProvider(props) {
-  const [loading, setLoading] = useState(false);
   const [planets, setPlanets] = useState([]);
 
-  const getPlanets = async () => {
-    setLoading(true);
-    try {
-      const response = await fetchPlanets();
-      setPlanets(response);
-    } catch (e) {
-      setLoading(false);
-      console.log(e.message);
-    }
-  };
+  useEffect(() => {
+    const getPlanets = async () => {
+      try {
+        const response = await fetchPlanets();
+        setPlanets(response);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getPlanets();
+  }, []);
 
   const { children } = props;
-  const contextValue = { loading, planets, getPlanets };
+  const contextValue = { planets, setPlanets };
+
   return (
     <PlanetsContext.Provider value={ contextValue }>
       { children }
