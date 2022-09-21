a5610d0adb58dc24f7c8fdcdfef7a2257390883d
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-21 14:21:28 -0300

    feat(SearchInput): :sparkles: Cria input do Search Input

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PlanetsContext from '../contexts/PlanetsContext';
 
 function SearchFilter() {
+  const { filter, setFilterByName } = useContext(PlanetsContext);
+
+  const filterPlanets = (value) => {
+    setFilterByName({ name: value });
+  };
+
   return (
     <label htmlFor="search-filter">
-      <input type="text" data-testid="name-filter" id="search-filter" />
+      Filtro
+      <input
+        type="text"
+        data-testid="name-filter"
+        id="search-filter"
+        value={ filter }
+        onChange={ (e) => filterPlanets(e.target.value) }
+      />
     </label>
   );
 }
