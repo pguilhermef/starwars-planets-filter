6075958d8177da6d6156b8c1c668b47431eb0f33
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-21 15:50:09 -0300

    feat(NumberFilter): :sparkles: Adiciona os selects e options do filtro de valores numericos

--- src/components/NumberFilter.js
+++ src/components/NumberFilter.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 function NumberFilter() {
   return (
-    <div>NumberFilter</div>
+    <>
+      <select data-testid="column-filter">
+        <option>population</option>
+        <option>orbital_period</option>
+        <option>diameter</option>
+        <option>rotation_period</option>
+        <option>surface_water</option>
+      </select>
+
+      <select data-testid="comparison-filter">
+        <option>maior que</option>
+        <option>menor que</option>
+        <option>igual a </option>
+      </select>
+
+      <label htmlFor="value-filter">
+        <input id="value-filter" data-testid="value-filter" />
+      </label>
+
+      <button type="button" data-testid="button-filter">Filtrar</button>
+    </>
   );
 }
 
