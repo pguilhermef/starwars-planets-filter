8889e2fb7f76fdde8f718f423499bee77348e84a
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-21 14:25:20 -0300

    feat(Table): :sparkles: Cria lógica de rendenizar os planetas filtrados na Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../contexts/PlanetsContext';
 
 function Table() {
-  const { planets } = useContext(PlanetsContext);
+  const { planets, filterByName } = useContext(PlanetsContext);
 
   const tableTitles = [
     'Name',
@@ -28,23 +28,26 @@ function Table() {
         </tr>
       </thead>
       <tbody>
-        {planets && planets.map((info) => (
-          <tr key={ info.name }>
-            <td>{info.name}</td>
-            <td>{info.rotation_period}</td>
-            <td>{info.orbital_period}</td>
-            <td>{info.diameter}</td>
-            <td>{info.climate}</td>
-            <td>{info.gravity}</td>
-            <td>{info.terrain}</td>
-            <td>{info.surface_water}</td>
-            <td>{info.population}</td>
-            <td>{info.films.map((film) => <p key={ film }>{ film }</p>)}</td>
-            <td>{info.created}</td>
-            <td>{info.edited}</td>
-            <td>{info.url}</td>
-          </tr>
-        ))}
+        {planets && planets
+          .filter((planet) => planet.name.toUpperCase()
+            .includes(filterByName.name.toUpperCase()))
+          .map((info) => (
+            <tr key={ info.name }>
+              <td>{info.name}</td>
+              <td>{info.rotation_period}</td>
+              <td>{info.orbital_period}</td>
+              <td>{info.diameter}</td>
+              <td>{info.climate}</td>
+              <td>{info.gravity}</td>
+              <td>{info.terrain}</td>
+              <td>{info.surface_water}</td>
+              <td>{info.population}</td>
+              <td>{info.films.map((film) => <p key={ film }>{ film }</p>)}</td>
+              <td>{info.created}</td>
+              <td>{info.edited}</td>
+              <td>{info.url}</td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );
