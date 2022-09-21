bee7929e7f074f8758c6a46e4a483d187e79375c
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-21 14:47:32 -0300

    feat(NumberFilter): :sparkles: Cria e rendeniza o componente NumberFilter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import NumberFilter from './components/NumberFilter';
 import SearchFilter from './components/SearchFilter';
 import Table from './components/Table';
 import PlanetsProvider from './contexts/PlanetsProvider';
@@ -8,6 +9,7 @@ function App() {
   return (
     <PlanetsProvider>
       <SearchFilter />
+      <NumberFilter />
       <Table />
     </PlanetsProvider>
   );
