0ceec2a66dc793d32d25528d645b42a09c406c88
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-21 14:21:28 -0300

    feat(SearchInput): :sparkles: Cria input do Search Input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SearchFilter from './components/SearchFilter';
 import Table from './components/Table';
 import PlanetsProvider from './contexts/PlanetsProvider';
 
 function App() {
   return (
     <PlanetsProvider>
+      <SearchFilter />
       <Table />
     </PlanetsProvider>
   );
