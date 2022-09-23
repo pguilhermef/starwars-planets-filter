f3ce379acf40ea0ce904e93c1962474ba1f86554
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-23 12:42:32 -0300

    feat(NumberFilter): :sparkles: Adiciona lógica de adição de filtros ao estado global

--- src/components/NumberFilter.js
+++ src/components/NumberFilter.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PlanetsContext from '../contexts/PlanetsContext';
 
 function NumberFilter() {
+  const { filterByNumericValues, setfilterByNumericValues } = useContext(PlanetsContext);
+
+  const filterPlanetsByNumericValues = ({ target }) => {
+    setfilterByNumericValues((prevState) => ({
+      ...prevState,
+      [target.id]: target.value,
+    }));
+  };
+
   return (
     <>
-      <select data-testid="column-filter">
+      <select
+        id="column"
+        data-testid="column-filter"
+        value={ filterByNumericValues.column }
+        onChange={ filterPlanetsByNumericValues }
+      >
         <option>population</option>
         <option>orbital_period</option>
         <option>diameter</option>
@@ -11,14 +26,24 @@ function NumberFilter() {
         <option>surface_water</option>
       </select>
 
-      <select data-testid="comparison-filter">
+      <select
+        id="comparison"
+        data-testid="comparison-filter"
+        value={ filterByNumericValues.comparison }
+        onChange={ filterPlanetsByNumericValues }
+      >
         <option>maior que</option>
         <option>menor que</option>
         <option>igual a </option>
       </select>
 
       <label htmlFor="value-filter">
-        <input id="value-filter" data-testid="value-filter" />
+        <input
+          id="value"
+          data-testid="value-filter"
+          value={ filterByNumericValues.value }
+          onChange={ filterPlanetsByNumericValues }
+        />
       </label>
 
       <button type="button" data-testid="button-filter">Filtrar</button>
