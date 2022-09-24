5b7657aea29d2c2ad23e85e8eff5851f72d18020
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-24 20:27:13 -0300

    test(Testes): :white_check_mark: Cria coberturas de teste para o primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import testData from '../../cypress/mocks/testData'
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+const NAME_FILTER = 'name-filter';
+const COLUMN_FILTER = 'column-filter';
+const COMPARISON_FILTER = 'comparison-filter';
+const VALUE_FILTER = 'value-filter';
+const BUTTON_FILTER = 'button-filter';
+
+describe('Requisito 1 - Requisição de API e preenchimento da tabela', () => {
+  test('Testa se é feita uma requisição a api', async () => {
+    global.fetch = jest.fn(async () => ({
+      json: async () => testData,
+    }))
+
+    render(<App />)
+
+    const API_CORRECT_FETCH = 'https://swapi.dev/api/planets';
+    const TATOOINE = await screen.findByText(/tatooine/i)
+
+    expect(TATOOINE).toBeInTheDocument()
+    expect(global.fetch).toHaveBeenCalled()
+    expect(global.fetch).toHaveBeenCalledWith(API_CORRECT_FETCH)
+  })
+
+  test('Testa se a tabela possui 13 colunas', async () => {
+    global.fetch = jest.fn(async () => ({
+      json: async () => testData,
+    }))
+
+    render(<App />)
+
+    const TABLE_COLUMNS = screen.getAllByRole("columnheader")
+    
+    expect(TABLE_COLUMNS).toHaveLength(13)
+  })
 });
