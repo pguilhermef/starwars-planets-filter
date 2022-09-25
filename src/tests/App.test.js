6a3d75a396fda35cf083ada8f119dc7654813f8a
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-24 21:40:38 -0300

    test(Testes): :white_check_mark: Cria casos de teste para o requisito 2 e 3

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 import testData from '../../cypress/mocks/testData'
+import userEvent from '@testing-library/user-event';
 
 const NAME_FILTER = 'name-filter';
 const COLUMN_FILTER = 'column-filter';
@@ -36,4 +37,59 @@ describe('Requisito 1 - Requisição de API e preenchimento da tabela', () => {
     
     expect(TABLE_COLUMNS).toHaveLength(13)
   })
+});
+
+describe('Requisito 2 e 3 - Criar filtros', () => {
+  test('Testa se os botões de filtros são rendenizados', async () => {
+    render(<App />)
+
+    const NAME_FILTER_INPUT = screen.getByTestId(NAME_FILTER)
+    const COLUMN_FILTER_SELECT = screen.getByTestId(COLUMN_FILTER)
+    const COMPARISON_FILTER_SELECT = screen.getByTestId(COMPARISON_FILTER)
+    const VALUE_FILTER_SELECT = screen.getByTestId(VALUE_FILTER)
+    const BUTTON_TO_FILTER = screen.getByTestId(BUTTON_FILTER)
+
+    expect(NAME_FILTER_INPUT).toBeInTheDocument()
+    expect(COLUMN_FILTER_SELECT).toBeInTheDocument()
+    expect(COMPARISON_FILTER_SELECT).toBeInTheDocument()
+    expect(VALUE_FILTER_SELECT).toBeInTheDocument()
+    expect(BUTTON_TO_FILTER).toBeInTheDocument()
+  })
+
+  test('Testa se é possível clicar no botão de filtrar', async () => {
+    global.fetch = jest.fn(async () => ({
+      json: async () => testData
+    }));
+
+    render(<App />);
+
+    const value = screen.getByTestId(VALUE_FILTER);
+    const button = screen.getByTestId(BUTTON_FILTER);
+
+    userEvent.type(value, '7');
+    expect(value).toHaveValue(7);
+
+    userEvent.click(button);
+  });
+
+  test('Testa se é possível selecionar diferentes opções no filtro "Comparison"', () => {
+    global.fetch = jest.fn (async () => ({
+      json: async () => testData,
+    }))
+
+    render(<App />)
+
+    const COMPARISON_FILTER_SELECT = screen.getByTestId(COMPARISON_FILTER)
+    const BUTTON_TO_FILTER = screen.getByTestId(BUTTON_FILTER)
+    
+    userEvent.selectOptions(COMPARISON_FILTER_SELECT, 'maior que')
+    expect(COMPARISON_FILTER_SELECT).toHaveValue('maior que')
+
+    userEvent.selectOptions(COMPARISON_FILTER_SELECT, 'menor que')
+    expect(COMPARISON_FILTER_SELECT).toHaveValue('menor que')
+
+    userEvent.selectOptions(COMPARISON_FILTER_SELECT, 'igual a')
+    expect(COMPARISON_FILTER_SELECT).toHaveValue('igual a')
+  })
+
 });
