f65b637fd2b101508d6cdfd6309c76f15ff8903c
pguilhermef <pguilhermesantos@hotmail.com>
2022-11-12 15:51:03 -0300

    style: Adiciona estilo aos filtros

--- components/NameFilter.js
+++ components/NameFilter.js
@@ -9,16 +9,22 @@ function NameFilter() {
   };
 
   return (
-    <label htmlFor="search-filter">
-      Filtro
-      <input
-        type="text"
-        data-testid="name-filter"
-        id="search-filter"
-        value={ filter }
-        onChange={ (e) => filterPlanetsByName(e.target.value) }
-      />
-    </label>
+    <div>
+      <div className="mt-1 flex rounded-md shadow-sm justify-center items-center">
+        <label htmlFor='search-filter' className='mr-2 '>
+          Planeta
+        </label>
+        <input
+          type="text"
+          data-testid="name-filter"
+          id="search-filter"
+          value={ filter }
+          onChange={ (e) => filterPlanetsByName(e.target.value) }
+          placeholder="  Tatooine"
+          className="rounded bg-gray-300 py-2 placeholder:italic placeholder"
+        />
+      </div>
+    </div> 
   );
 }
 
