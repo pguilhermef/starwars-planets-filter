8442b8ff25db5b03d5e6b40ae20134b17b1492d4
pguilhermef <pguilhermesantos@hotmail.com>
2022-11-12 13:05:07 -0300

    style:
    Adiciona logo starwars na rendenização

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,19 @@ import NameFilter from '../components/NameFilter'
 import NumberFilter from '../components/NumberFilter'
 import Table from '../components/Table'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import starWars from '../public/images/StarWars.png'
 
 export default function Home() {
   return (
     <PlanetsProvider>
       <div className={styles.container}>
-        <NameFilter />
-        <NumberFilter />
-        <Table />
+        <body>
+          <Image src={starWars} alt='star wars logo' className='md:mb-6'/>
+          <NameFilter />
+          <NumberFilter />
+          <Table />
+        </body>
       </div>
     </PlanetsProvider>
   )
