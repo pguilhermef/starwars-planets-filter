04751d7fddee82fb00acd74eba58134bd316a7bf
Eiji Tomonari <gabriel@tomonari.dev>
2022-11-16 18:56:11 -0300

    Fix hydration error

--- contexts/PlanetsProvider.js
+++ contexts/PlanetsProvider.js
@@ -1,27 +1,26 @@
-import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
-import PlanetsContext from './PlanetsContext';
-import fetchPlanets from '../services/fetchPlanets';
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import PlanetsContext from "./PlanetsContext";
+import fetchPlanets from "../services/fetchPlanets";
 
 function PlanetsProvider(props) {
   const [planets, setPlanets] = useState([]);
   const [planetsToFilter, setPlanetsToFilter] = useState([]);
   const [filterByName, setFilterByName] = useState({
-    name: '',
+    name: "",
   });
   const [filterByNumericValues, setfilterByNumericValues] = useState([]);
   const [filterButton, setFilterButton] = useState(false);
   const columnsAvailableInitialState = [
-    'population',
-    'orbital_period',
-    'diameter',
-    'rotation_period',
-    'surface_water',
+    "population",
+    "orbital_period",
+    "diameter",
+    "rotation_period",
+    "surface_water",
   ];
-  const [
-    columnFilterAvailable,
-    setColumnFilterAvailable,
-  ] = useState(columnsAvailableInitialState);
+  const [columnFilterAvailable, setColumnFilterAvailable] = useState(
+    columnsAvailableInitialState
+  );
 
   // Chama a api e atribui seu resultado a planets e planetsToFilter
   useEffect(() => {
@@ -51,13 +50,14 @@ function PlanetsProvider(props) {
   // Quando o .filter no useEffect abaixo for ativado, retorna planetas que derem True
   const handleComparisonFilter = (planet, column, comparison, value) => {
     switch (comparison) {
-    case 'maior que':
-      return Number(planet[column]) > Number(value);
-    case 'menor que':
-      return Number(planet[column]) < Number(value);
-    case 'igual a':
-      return Number(planet[column]) === Number(value);
-    default: return planet;
+      case "maior que":
+        return Number(planet[column]) > Number(value);
+      case "menor que":
+        return Number(planet[column]) < Number(value);
+      case "igual a":
+        return Number(planet[column]) === Number(value);
+      default:
+        return planet;
     }
   };
 
@@ -67,23 +67,22 @@ function PlanetsProvider(props) {
     if (filterByNumericValues.length > 0) {
       filterByNumericValues.forEach((filter) => {
         const { column, comparison, value } = filter;
-        const filteredPlanetList = filterPlanets
-          .filter((planet) => (
-            handleComparisonFilter(planet, column, comparison, value)
-          ));
+        const filteredPlanetList = filterPlanets.filter((planet) =>
+          handleComparisonFilter(planet, column, comparison, value)
+        );
         filterPlanets = filteredPlanetList;
       });
     }
     // columnsAvailables();
     // Problema da mentoria está aqui!
     // Ao excluir o filtro, ele nao apaga
-    console.log('fui atualizado');
+    console.log("fui atualizado");
     setPlanetsToFilter(filterPlanets);
   }, [filterButton]); // eslint-disable-line
 
-  const filteredPlanets = planetsToFilter
-    .filter((planet) => planet.name.toUpperCase()
-      .includes(filterByName.name.toUpperCase()));
+  const filteredPlanets = planetsToFilter.filter((planet) =>
+    planet.name.toUpperCase().includes(filterByName.name.toUpperCase())
+  );
 
   const { children } = props;
   const contextValue = {
@@ -102,8 +101,8 @@ function PlanetsProvider(props) {
   };
 
   return (
-    <PlanetsContext.Provider value={ contextValue }>
-      { children }
+    <PlanetsContext.Provider value={contextValue}>
+      {children}
     </PlanetsContext.Provider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,26 @@
-import PlanetsProvider from '../contexts/PlanetsProvider'
-import NameFilter from '../components/NameFilter'
-import NumberFilter from '../components/NumberFilter'
-import Table from '../components/Table'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
-import starWars from '../public/images/StarWars.png'
+import PlanetsProvider from "../contexts/PlanetsProvider";
+import NameFilter from "../components/NameFilter";
+import NumberFilter from "../components/NumberFilter";
+import Table from "../components/Table";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import starWars from "../public/images/StarWars.png";
 
 export default function Home() {
   return (
     <PlanetsProvider>
       <div className={styles.container}>
-        <body className='flex flex-col justify-center items-center'>
-          <div>
-            <Image src={starWars} alt='star wars logo' className='mt-10 mb-1'/>
-          </div>
-          <div className='bg-white p-2 w-full rounded flex justify-evenly items-center' >
-            <NameFilter />
-            <NumberFilter />
-          </div>
-          <div>
-            <Table />
-          </div>
-        </body>
+        <div>
+          <Image src={starWars} alt="star wars logo" className="mt-10 mb-1" />
+        </div>
+        <div className="bg-white p-2 w-full rounded flex justify-evenly items-center">
+          <NameFilter />
+          <NumberFilter />
+        </div>
+        <div>
+          <Table />
+        </div>
       </div>
     </PlanetsProvider>
-  )
+  );
 }
