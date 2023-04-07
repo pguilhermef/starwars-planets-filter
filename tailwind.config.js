fa5261ae9a1d9de54878277c89c58124983f97eb
pguilhermef <pguilhermesantos@hotmail.com>
2023-04-07 20:32:53 -0300

    style: Create custom colors theme based in starwars

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        starwars: {
+          yellow: '#FFE81F',
+          red: '#EB212E',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
